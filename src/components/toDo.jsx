e58a2a63a13d8ecb3bca32a0f4e8d01a51a2591e
zafarkh <hatamovzafar7070@gmail.com>
2024-01-26 23:03:56 +0500

    'some_changes'

--- src/components/toDo.jsx
+++ src/components/toDo.jsx
@@ -1,46 +1,3 @@
-// import React, { useState } from "react";
-
-// const ToDo = () => {
-//   const [todos, setTodos] = useState([]);
-//   const [value, setValue] = useState("");
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     setTodos([...todos, value]);
-//     setValue("");
-//   };
-
-//   const handleDelete = (index) => {
-//     const newTodos = [...todos];
-//     newTodos.splice(index, 1);
-//     setTodos(newTodos);
-//   };
-
-//   return (
-//     <>
-//       <h1>To Do List</h1>
-//       <form>
-//         <input
-//           type="text"
-//           value={value}
-//           onChange={(e) => setValue(e.target.value)}
-//         />
-//         <button onClick={handleSubmit}>Add Todo</button>
-//       </form>
-//       <ul>
-//         {todos.map((todo, index) => (
-//           <li key={index}>
-//             {todo}
-//             <button onClick={() => handleDelete(index)}>Delete</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// };
-
-// export default ToDo;
-
 import React, { useState, useRef } from "react";
 import { v4 as uuid } from "uuid";
 
@@ -50,20 +7,21 @@ const ToDo = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setTodos([
-      ...todos,
-      {
-        text: value.current.value,
-        id: uuid(),
-      },
-    ]);
-    value.current.value = "";
+    if (!value.current.value) {
+      return 0;
+    } else {
+      setTodos([
+        ...todos,
+        {
+          text: value.current.value,
+          id: uuid(),
+        },
+      ]);
+      value.current.value = "";
+    }
   };
 
   const handleDelete = (id) => {
-    // const newTodos = [...todos];
-    // newTodos.splice(index, 1);
-    // setTodos(newTodos);
     setTodos((currentTodo) => {
       return currentTodo.filter((todo) => todo.id !== id);
     });
